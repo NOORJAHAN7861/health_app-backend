adef6c6af3c831168d94a08e0d3bab080dbf826d
unknown <noorjahanpp786@gmail.com>
2026-04-18 15:45:14 +0530

    .

--- models/userSchema.js
+++ models/userSchema.js
@@ -63,20 +63,22 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next();
+    return next();   // ✅ must RETURN
   }
+
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.methods.generateJsonWebToken = function () {   
-  return jwt.sign(    
+userSchema.methods.generateJsonWebToken = function () {
+  return jwt.sign(
     { id: this._id },
     process.env.JWT_SECRET_KEY,
-    { expiresIn: process.env.COOKIE_EXPIRY }
+    { expiresIn: process.env.JWT_EXPIRE }
   );
 };
 
